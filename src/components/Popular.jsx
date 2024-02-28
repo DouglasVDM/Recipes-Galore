b5b871ec5c65692a6df0ec8791fc78ba4e6f5d78
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-28 19:41:19 +0200

    Document format update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,128 +1,107 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/react-splide/css';
-import { Link } from "react-router-dom";
-
-
-
+import "@splidejs/react-splide/css";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
 
-    const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
 
-    useEffect(() => {
-        getPopular();
-    }, []);
+  const getPopular = async () => {
+    const check = localStorage.getItem("popular");
+    console.log("check", check);
 
-    const getPopular = async () => {
+    if (check && check !== "[object JSON]") {
+      // Valid JSON data exists in local storage
+        setPopular(JSON.parse(check));
+    } else {
+      // Fetch new data from API
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
+      );
+      const data = await api.json();
+      console.log("data", data);
 
-        const check = localStorage.getItem('popular');
-
-        if (check && check !== "[object JSON]") {
-          // Valid JSON data exists in local storage
-          setPopular(JSON.parse(check));
-        } else {
-          // Fetch new data from API
-          const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
-          );
-          const data = await api.json();
-        
-          // Store valid JSON data in local storage
-          localStorage.setItem('popular', JSON.stringify(data.recipes));
-          setPopular(data.recipes);
-        }
-
-       
-        
-       
+      // Store valid JSON data in local storage
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
     }
 
-
     return (
-        <div>
-            <Wrapper>
-                <h3>Trending Recipes</h3>
-                <Splide
-                options={{
-                    perPage: 5,
-                    arrows: true,
-                    pagination: false,
-                    drag: "free",
-                    gap: "5rem",
-                }}
-                >
+      <div>
+        <Wrapper>
+          <h3>Trending Recipes</h3>
+          <Splide
+            options={{
+              perPage: 5,
+              arrows: true,
+              pagination: false,
+              drag: "free",
+              gap: "5rem",
+            }}
+          >
+            {popular.map((recipe) => {
+              return (
+                <SplideSlide key={recipe.id}>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Card>
+                </SplideSlide>
+              );
+            })}
+          </Splide>
+        </Wrapper>
+      </div>
+    );
+  };
 
-                    {popular.map((recipe) => {
-                        return (
-                            <SplideSlide key={recipe.id}>
-
-                                <Card>
-                                    <Link to={"/recipe/" + recipe.id}>
-                                    <p>{recipe.title}</p>
-                                    <img src={recipe.image} alt={recipe.title} />
-                                    <Gradient />
-                                    </Link>
-                                </Card>
-                            </SplideSlide>
-                        );
-                    })}
-                </Splide>
-            </Wrapper>
-        </div>
-    )
-}
-
-const Wrapper = styled.div` 
-margin: 2rem 2rem;
-`;
-const Card = styled.div`
-min-height: 25rem;
-border-radius: 2rem;
-overflow: hidden;
-position: relative;
-border-style: solid;
-border-color: #d9e3eb;
-
-img{
+  const Wrapper = styled.div`
+    margin: 2rem 2rem;
+  `;
+  const Card = styled.div`
+    min-height: 25rem;
     border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+      border-radius: 2rem;
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    p {
+      position: absolute;
+      z-index: 10;
+      left: 50%;
+      bottom: 0%;
+      transform: translate(-50%, 0%);
+      color: white;
+      width: 100%;
+      text-align: center;
+      font-weight: 600;
+      font-size: 1rem;
+      height: 40%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `;
+
+  const Gradient = styled.div`
+    z-index: 3;
     position: absolute;
-    left: 0;
     width: 100%;
     height: 100%;
-    object-fit: cover;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+  `;
 }
-
-p{
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-}
-
-`;
-
-const Gradient = styled.div`
-z-index: 3;
-position: absolute;
-width: 100%;
-height: 100%;
-background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
-`;
-
-export default Popular
-
-
-
+export default Popular;
--- src/pages/Diet.jsx
+++ src/pages/Diet.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect,  useState } from 'react';
 import styled from 'styled-components';
 // import { motion } from 'framer-motion';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function Diet() {
 
